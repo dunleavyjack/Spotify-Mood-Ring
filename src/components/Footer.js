25a56a2b1186d5e8cd432c215f341758d45145ab
Jack Joseph <dunleavyjack@gmail.com>
2021-02-16 00:09:44 +0900

    Added a ":)" to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
         <footer className="d-flex align-items-center justify-content-center position-fixed">
             <p className="text-muted mt-3">
-                Built by <a className="no-decoration white" href="https://github.com/dunleavyjack">@jackjoseph</a>
+                Built by <a className="no-decoration white" href="https://github.com/dunleavyjack">@jackjoseph</a> :)
             </p>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer;
+export default Footer:
