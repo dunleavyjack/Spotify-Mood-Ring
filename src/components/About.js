6b7f1e66408ba7058783f7e9b8dc8e934bc8d023
Jack Joseph <dunleavyjack@gmail.com>
2021-02-18 11:25:26 +0900

    Updated about page info

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,14 @@ const About = () => {
             <Navbar />
                 <div className="tan d-flex justify-content-center align-items-center content-body text-center content-body">
                     <div className="container flex-fill tan">
-                        <h1 className="bold">About</h1>
-                        <h3 className="bold">{`Hi! This project was built using `}
+                        <h1 className="bold about-header">About</h1>
+                        <h3>Your mood is predicted using track analytics from the Spotify API.</h3>
+                        
+                        <h1 className="bold about-header mt-5">Privacy</h1>
+                        <h3>Your account data remains completely private and cannot be viewed, shared, or saved by anyone. You can view the complete Spotify privacy policy <a className="custom-link" href="https://www.spotify.com/us/legal/privacy-policy/">here</a>.</h3>
+                        
+                        <h1 className="bold about-header mt-5">Development</h1>
+                        <h3>{`This project was built using `}
                             <a className="custom-link" href="https://reactjs.org/">React</a>
                             {', '}
                             <a className="custom-link" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/">Bootstrap 5</a>
--- src/components/MoodResultDisplay.js
+++ src/components/MoodResultDisplay.js
@@ -5,9 +5,7 @@ const MoodResultDisplay = ({mood}) => {
         <div className="tan">
             {/* <p className="tan floating">&#128522;</p> */}
             <h1 className="title-small">{mood.name}</h1>
-            {/* <h1 className="title-small">{mood.name}</h1> */}
-            <p className="bold">Your recent songs have<span className="pinkish large-text"> {mood.firstMood.difference.toFixed(2)}%</span> {mood.firstMood.mood.replace("-", " ")} and <span className="pinkish large-text"> {mood.secondMood.difference.toFixed(2)}%</span> {mood.secondMood.mood.replace("-", " ")} than the average.</p>
-            {/* <p>They are {mood.firstMood.difference} and {mood.secondMood.difference}</p> */}
+            <p className="bold">Your recent songs have<span className="pinkish large-text"> {mood.firstMood.difference.toFixed(2)}%</span> {mood.firstMood.mood.replace("-", " ")} and <span className="pinkish large-text"> {mood.secondMood.difference.toFixed(2)}%</span> {mood.secondMood.mood.replace("-", " ")} than average.</p>
         </div>
     )
 }
