ba263b5e325146756e9a3ca638b7be72df366b33
Jack Joseph <dunleavyjack@gmail.com>
2021-02-21 13:26:47 +0900

    Updated about (again)

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
                     <h3>Your account data cannot be viewed, shared, or saved by anyone. You can view the complete Spotify privacy policy <a className="custom-link" href="https://www.spotify.com/us/legal/privacy-policy/">here</a>.</h3>
 
                     <h1 className="bold about-header mt-3">Development &#x1F468;&#x200D;&#x1F4BB;</h1>
-                    <h3>This project was and built using
+                    <h3>This project was built using
                             <a className="custom-link" href="https://reactjs.org/"> React</a>
                         {', '}
                         <a className="custom-link" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/">Bootstrap 5</a>
