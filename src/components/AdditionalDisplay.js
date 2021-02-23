21b23fd39c599b2a7d2e87caec94d079053c204c
Jack Joseph <dunleavyjack@gmail.com>
2021-02-23 13:12:26 +0900

    Added scores for full breakdown component

--- src/components/AdditionalDisplay.js
+++ src/components/AdditionalDisplay.js
@@ -7,7 +7,28 @@ const AdditionalDisplay = () => {
                 <div className="whiteish">
                     <div className="black-body">
                         <div className="container-fluid black-body">
-                            <h3 className="bold mt-5 mb-5">Your Full Breakdown</h3>
+                            <h1 className="bold mt-5 mb-5 tan-font">Your Full Breakdown</h1>
+                            
+                            <div className="black-body row">
+                                <h4 className="col mt-auto bold">Higher Danceability</h4>
+                                <h4 className="col bold floating pinkish">24.54%</h4>
+                            </div>
+
+                            <div className="black-body row">
+                                <h4 className="col mt-auto bold">Higher Valence</h4>
+                                <h4 className="col bold floating pinkish">6.56%</h4>
+                            </div>
+
+                            <div className="black-body row">
+                                <h4 className="col mt-auto bold">Lower Acousticness</h4>
+                                <h4 className="col bold floating pinkish">15.06%</h4>
+                            </div>
+
+                            <div className="black-body row mb-5">
+                                <h4 className="col mt-auto bold">Higher Volume</h4>
+                                <h4 className="col bold floating pinkish">15.06%</h4>
+                            </div>
+
                         </div>
                     </div>
                 </div>
