82cb051af8662d5ada3099330567ce4a9107dc79
Jack Joseph <dunleavyjack@gmail.com>
2021-02-23 11:05:40 +0900

    Styling additional display

--- src/components/AdditionalDisplay.js
+++ src/components/AdditionalDisplay.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const AdditionalDisplay = () => {
     return (
-        <div>
-            <div className="black-body pinkish">Hi</div>
-        </div>
-
+        <div className="container-fluid black-body">
+            <div className="black-body d-flex justify-content-center text-center">
+                <div className="whiteish">
+                    <div className="black-body">
+                        <div className="container-fluid black-body">
+                            <h3 className="bold mt-5 mb-5">Your Full Breakdown</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
