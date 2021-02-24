5581d593c49bdd80c352db25982a27945c315555
Jack Joseph <dunleavyjack@gmail.com>
2021-02-25 00:52:06 +0900

    Tweaked the spacing

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,8 @@ const About = () => {
                     <h5 className="black-body whiteish">
                         This project was built with
                         <a className="custom-link" href="https://reactjs.org/"> React</a>,
-                        <a className="custom-link" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/"> Bootstrap 5</a>,
-                        <a className="custom-link" href="https://developer.spotify.com/documentation/web-api/"> the Spotify Web API </a> and
+                        <a className="custom-link" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/"> Bootstrap 5</a>, the 
+                        <a className="custom-link" href="https://developer.spotify.com/documentation/web-api/"> Spotify Web API </a> and
                         designed in
                         <a className="custom-link" href="https://www.figma.com/design/"> Figma</a>.
                         It's also hosted with
@@ -23,14 +23,14 @@ const About = () => {
                     </h5>
 
                     <h1 className="bold about-header mt-5 black-body whiteish">privacy</h1>
-                    <h5 className="mb-5 black-body whiteish">The complete Spotify privacy policy can be viewed here <a className="custom-link" href="https://www.spotify.com/us/legal/privacy-policy/">here</a>, which also covers approved third-party projects like this one. </h5>
+                    <h5 className="mb-5 black-body whiteish">The complete Spotify privacy policy can be viewed <a className="custom-link" href="https://www.spotify.com/us/legal/privacy-policy/">here</a>, which also covers approved third-party projects like this one. </h5>
 
                     <h1 className="bold about-header mt-5 black-body whiteish">mood detecting</h1>
                     <h5 className="black-body whiteish mb-5">Your mood is determined based on different analytics (tempo, energy, acousticness, etc.) from each song. An average is found from your recent music which is compared with the levels of an 'average listener'. That data is matched with one of over thirty moods and that's what you see. <br /><br /> Moods are, of course, strange and ineffable. But hopefully it connected with you :)</h5>
 
                     <h1 className="bold about-header mt-5 black-body whiteish">contact</h1>
                     <h5 className="black-body whiteish">dunleavyjack@gmail.com</h5>
-                    <h5 className="black-body whiteish mb-5">Seoul :)</h5>
+                    <h5 className="black-body whiteish mb-5">Seoul 2021 :)</h5>
                 </div>
             </div>
         </div>
