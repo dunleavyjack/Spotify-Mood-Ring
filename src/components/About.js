3866596cebaa898ac9f8dfbfecdf81aedd08589a
Jack Joseph <dunleavyjack@gmail.com>
2021-02-24 13:57:35 +0900

    Tan font

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <Navbar />
             <div className="black-body d-flex justify-content-center align-items-center content-body text-center content-body">
                 <div className="container flex-fill mt-5">
-                    <h1 className="bold about-header mt-5 black-body whiteish">About</h1>
+                    <h1 className="bold about-header mt-5 black-body tan-font">About</h1>
                     
                     <h5 className="black-body whiteish">
                         This project was built with
@@ -21,10 +21,10 @@ const About = () => {
                         <a className="custom-link" href="https://github.com/dunleavyjack/Spotify-Mood-Ring"> here</a> on GitHub for anyone who wants to take a look.
                     </h5>
                     
-                    <h1 className="bold about-header mt-5 black-body whiteish">The Mood Detector</h1>
+                    <h1 className="bold about-header mt-5 black-body tan-font">The Mood Detector</h1>
                     <h5 className="black-body whiteish">Your mood is determined based on different analytics (tempo, energy, acousticness, etc.) from each song. An average is found from your recent music which is compared with the levels of an 'average listener'. That data is matched with one of over thirty moods and that's what you see. Moods are strange and ineffable, but hopefully it connected with you :)</h5>
                     
-                    <h1 className="bold about-header mt-5 black-body whiteish">Privacy</h1>
+                    <h1 className="bold about-header mt-5 black-body tan-font">Privacy</h1>
                     <h5 className="mb-5 black-body whiteish">For obvious reasons, your account data cannot be viewed, shared, or saved by anyone. You can view the complete Spotify privacy policy <a className="custom-link" href="https://www.spotify.com/us/legal/privacy-policy/">here</a>, which also covers approved third-party projects like this one. </h5>
                 </div>
             </div>
