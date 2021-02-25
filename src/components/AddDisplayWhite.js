8abed8ca3e02357d507a98c8b07b87546cebe960
Jack Joseph <dunleavyjack@gmail.com>
2021-02-25 14:37:29 +0900

    Testing animation changes

--- src/components/AddDisplayWhite.js
+++ src/components/AddDisplayWhite.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AddDisplayWhite = ({label, percent}) => {
     return (
         <React.Fragment>
-            <h4 className="bold floating whiteish">{percent}</h4>
+            <h4 className="bold floating-two whiteish">{percent}</h4>
             <h4 className="bold whiteish mt-3 mb-5">{label}</h4>
         </React.Fragment>
     )
