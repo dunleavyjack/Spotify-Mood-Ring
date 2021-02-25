0f3a810626aad415e4061c2118b963373d927020
Jack Joseph <dunleavyjack@gmail.com>
2021-02-25 14:45:34 +0900

    Possibly finished animations?

--- src/components/MoodResultDisplay.js
+++ src/components/MoodResultDisplay.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MoodResultDisplay = ({ mood: {name, firstMood, secondMood, conjuction}}) => {
     return(
-        <div className="tan">
+        <div className="tan mt-3">
             {/* <p className="tan floating">&#128522;</p> */}
             <h1 className="title-small">{name}</h1>
             <p className="bold mb-5 fixed-container">Your recent songs have<span className="pinkish large-text"> {firstMood.difference.toFixed(2)}%</span> {firstMood.mood.replace("-", " ")} {conjuction} <span className="pinkish large-text"> {secondMood.difference.toFixed(2)}%</span> {secondMood.mood.replace("-", " ")} than average.</p>
