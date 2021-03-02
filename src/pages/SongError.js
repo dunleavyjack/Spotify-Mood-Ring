6c4e4142aba3f5390a4970a44a0c3b34e37d6589
Jack Joseph <dunleavyjack@gmail.com>
2021-03-02 23:25:24 +0900

    fixed typo

--- src/pages/SongError.js
+++ src/pages/SongError.js
@@ -9,7 +9,7 @@ const NotFound = () => {
                 <div className="container">
                     <h3 className="mt-3">Sorry, there was an error loading your songs.</h3>
                     <h4><span className="pinkish tan">:(</span></h4>
-                    <h4 className="mt-3">You might be using Spotify with a private session, or you might not have listened to enough songs recently for the program to work.</h4>
+                    <h4 className="mt-3">You might be using Spotify with a Private Session, or you might not have listened to enough songs recently for the program to work.</h4>
 
                 </div>
             </div>
