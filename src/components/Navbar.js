b0d9766646b7caba8d6b6ec68219885360c03fee
dunleavyjack <dunleavyjack@gmail.com>
2021-03-27 09:45:28 +0900

    Changed from beta

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <Link className="navbar-brand nav-title brand-text" to="/"><span className="pinkish">mood ring</span> {'{'} beta {'}'} </Link>
+                <Link className="navbar-brand nav-title brand-text" to="/"><span className="pinkish">mood ring &#57406;</span></Link>
                 <div className="navbar-nav ml-auto">
                     <Link className="navbar-brand nav-title brand-text" to="/about">about</Link>
                 </div>
