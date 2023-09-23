59fa0694c8610f131b69f3ae17e75c1e5b289d6c
dunleavyjack <dunleavyjack@gmail.com>
2023-09-23 11:43:06 -0500

    feat(setup): added react-native

---
+++ apps/moodring-mobile/jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  displayName: 'moodring-mobile',
+  preset: 'react-native',
+  resolver: '@nx/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  moduleNameMapper: {
+    '\\.svg$': '@nx/react-native/plugins/jest/svg-mock',
+  },
+  coverageDirectory: '../../coverage/apps/moodring-mobile',
+};
