caa56ad6c5ad980416f45918f73a71ad9b22c2fe
dunleavyjack <dunleavyjack@gmail.com>
2023-09-23 11:29:49 -0500

    feat(setup): created nx monorepo

--- apps/moodring-web/src/app/app.tsx
+++ apps/moodring-web/src/app/app.tsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
-      <NxWelcome title="moodring-web" />
+      <h1>Hello from moodring</h1>
     </div>
   );
 }
