96af890b8b609711ef9a34810777e423eadbec80
dunleavyjack <dunleavyjack@gmail.com>
2023-09-23 11:10:30 -0500

    Initial commit

---
+++ apps/moodring-web/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="moodring-web" />
+    </div>
+  );
+}
+
+export default App;
