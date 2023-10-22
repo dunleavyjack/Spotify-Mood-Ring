eed9314569f344dd83aeb7d5302dff153dc68496
dunleavyjack <dunleavyjack@gmail.com>
2023-10-22 12:31:34 -0400

    chore(frontend): updated index.js and main.tsx

--- apps/moodring-web/src/main.tsx
+++ apps/moodring-web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import { store } from '@moodring/store';
 
 const root = ReactDOM.createRoot(
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <Provider store={store}>
+    <StoreProvider store={store}>
       <App />
-    </Provider>
+    </StoreProvider>
   </StrictMode>
 );
